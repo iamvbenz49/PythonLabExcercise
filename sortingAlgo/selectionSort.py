43ce45c3c4ea049f8778130428b0edbf5d305469
Sham Vijay <benzwrites@gmail.com>
2023-03-06 06:56:20 +0530

    Fixed a typo

--- sortingAlgo/selectionSort.py
+++ sortingAlgo/selectionSort.py
@@ -1,7 +1,7 @@
 l = [1,2,3,4,5,6,7,8,9]
 for i in range(len(l)-1):
   min_idx = i
-  for j in range(i+1,len(l)-1):
+  for j in range(i+1,len(l)):
     if(l[min_idx]>l[j]):
       min_idx = j
   l[min_idx],l[i] = l[i],l[min_idx]
